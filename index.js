2d467404f34338d97189e22a96dace9397892fc6
Samantha <finalphoenix@Samanthas-MacBook.local>
2019-03-09 11:56:10 +0800

    cleaning out the js files

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 'use strict';
-const uuidv1 = require('uuid/v1');
-
-const APIAI_TOKEN = '4db5ced9eff540d18738f0bf2ee9f095';
-const APIAI_SESSION_ID = uuidv1();
-
 const express = require('express');
 const app = express();
 
@@ -12,39 +7,4 @@ app.use(express.static(__dirname + '/public')); // js, css, images
 
 const server = app.listen(process.env.PORT || 5000, () => {
   console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
-});
-
-const io = require('socket.io')(server);
-io.on('connection', function(socket){
-  console.log('a user connected');
-});
-
-const apiai = require('apiai')(APIAI_TOKEN);
-
-// Web UI
-app.get('/', (req, res) => {
-  res.sendFile('index.html');
-});
-
-io.on('connection', function(socket) {
-  socket.on('chat message', (text) => {
-    console.log('Message: ' + text);
-
-    let apiaiReq = apiai.textRequest(text, {
-      sessionId: APIAI_SESSION_ID
-    });
-
-    apiaiReq.on('response', (response) => {
-      let aiText = response.result.fulfillment.speech;
-      console.log('Bot reply: ' + aiText);
-      socket.emit('bot reply', aiText);
-    });
-
-    apiaiReq.on('error', (error) => {
-      console.log(error);
-    });
-
-    apiaiReq.end();
-
-  });
 });
